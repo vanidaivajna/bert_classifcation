f612f0a308c627551de259d4f02edebf00e52eab
Vani Daivajna <vanidaivajna16@gmail.com>
2023-05-17 09:04:39 +0530

    Create b3.py

---
+++ b3.py
@@ -0,0 +1,92 @@
+import torch
+import pandas as pd
+from transformers import BertTokenizer, BertForSequenceClassification
+from torch.utils.data import DataLoader, Dataset
+
+# Load and preprocess data
+train_df = pd.read_csv('train.csv')
+test_df = pd.read_csv('test.csv')
+
+class IMDbDataset(Dataset):
+    def __init__(self, dataframe, tokenizer, max_length):
+        self.tokenizer = tokenizer
+        self.max_length = max_length
+        self.texts = dataframe['text'].tolist()
+        self.labels = dataframe['label'].tolist()
+
+    def __len__(self):
+        return len(self.texts)
+
+    def __getitem__(self, index):
+        text = self.texts[index]
+        label = self.labels[index]
+
+        encoding = self.tokenizer.encode_plus(
+            text,
+            add_special_tokens=True,
+            max_length=self.max_length,
+            padding='max_length',
+            truncation=True,
+            return_tensors='pt'
+        )
+
+        input_ids = encoding['input_ids'].squeeze()
+        attention_mask = encoding['attention_mask'].squeeze()
+
+        return {
+            'input_ids': input_ids,
+            'attention_mask': attention_mask,
+            'label': torch.tensor(label)
+        }
+
+tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+max_length = 128
+
+train_dataset = IMDbDataset(train_df, tokenizer, max_length)
+test_dataset = IMDbDataset(test_df, tokenizer, max_length)
+
+train_dataloader = DataLoader(train_dataset, batch_size=16, shuffle=True)
+test_dataloader = DataLoader(test_dataset, batch_size=16, shuffle=False)
+
+# Load and fine-tune BERT model
+model = BertForSequenceClassification.from_pretrained('bert-base-uncased', num_labels=2)
+
+device = torch.device('cpu')
+model.to(device)
+
+optimizer = torch.optim.AdamW(model.parameters(), lr=2e-5)
+
+num_epochs = 5
+model.train()
+for epoch in range(num_epochs):
+    for batch in train_dataloader:
+        input_ids = batch['input_ids'].to(device)
+        attention_mask = batch['attention_mask'].to(device)
+        labels = batch['label'].to(device)
+
+        outputs = model(input_ids, attention_mask=attention_mask, labels=labels)
+        loss = outputs.loss
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+# Evaluation
+model.eval()
+total_correct = 0
+total_samples = 0
+
+with torch.no_grad():
+    for batch in test_dataloader:
+        input_ids = batch['input_ids'].to(device)
+        attention_mask = batch['attention_mask'].to(device)
+        labels = batch['label'].to(device)
+
+        outputs = model(input_ids, attention_mask=attention_mask)
+        predicted_labels = torch.argmax(outputs.logits, dim=1)
+
+        total_correct += (predicted_labels == labels).sum().item()
+        total_samples += labels.size(0)
+
+accuracy = total_correct / total_samples
+print(f'Test Accuracy: {accuracy:.2f}')
